068f95f56da0ef44ec145c8057cc52798611b14e
KondoA9 <kondo.eque@gmail.com>
2022-05-17 19:58:03 +0900

    chore(mac): fix the setup script

--- scripts/macos/setup.sh
+++ scripts/macos/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 running_path=`pwd`
 script_path=$(cd $(dirname $0); pwd)
-repo_path=`readlink -f ${script_path}/../../`
+repo_path="${script_path}/../../"
 
 # Workaround for compiling error of libtiff
 script_path=$(cd $(dirname $0); pwd)
