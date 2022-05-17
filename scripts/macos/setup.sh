575b972e54b25642ee33fd3660f943e9c3cd0fcb
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2022-05-17 18:59:25 +0900

    chore(script): clean library repo after building

--- scripts/macos/setup.sh
+++ scripts/macos/setup.sh
@@ -20,6 +20,11 @@ install_library() {
     # Copy shared object to bin/
     mkdir -p ${repo_path}/bin/
     cp ${repo_path}/deps/$1/build/$2*.dylib ${repo_path}/bin/
+
+    # Clean library repo
+    cd ..\
+    git restore *
+    git clean -f -d
 }
 
 # Build and install ZLIB
--- scripts/windows/setup.ps1
+++ scripts/windows/setup.ps1
@@ -7,12 +7,18 @@ function InstallLibrary ($library) {
     Set-Location ${REPO_PATH}deps\${library}
     mkdir build -Force
     Set-Location .\build\
+    cmake ..
     cmake --build . --config Release
     cmake --install .
 
     # Copy shared object to bin/
     mkdir ${REPO_PATH}bin -Force
     Copy-Item ${REPO_PATH}deps\${library}\build\Release\${library}.dll ${REPO_PATH}bin\
+
+    # Clean library repo
+    Set-Location ..\
+    git restore *
+    git clean -f -d
 }
 
 # Build and install ZLIB
