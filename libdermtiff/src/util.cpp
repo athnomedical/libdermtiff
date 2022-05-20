8b9100e6fd685a1efc7cfeb704e690f9cd9537b3
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2022-05-20 19:17:38 +0900

    fix(util): GetField<uint16,uint32>() returns 0 if failed

--- libdermtiff/src/util.cpp
+++ libdermtiff/src/util.cpp
@@ -9,6 +9,28 @@ namespace ldt::util {
                 TIFFClose(tiff);
             }
         }
+
+        template <typename T>
+        T GetFieldUInt(TIFF* const tiff, uint32_t tag) noexcept {
+            static_assert(std::is_unsigned<T>::value);
+            if (tiff == nullptr) {
+                return 0;
+            } else {
+                T value;
+                TIFFGetField(tiff, tag, &value);
+                return value;
+            }
+        }
+    }
+
+    template <>
+    uint16_t GetField(TIFF* const tiff, uint32_t tag) noexcept {
+        return _internal::GetFieldUInt<uint16_t>(tiff, tag);
+    }
+
+    template <>
+    uint32_t GetField(TIFF* const tiff, uint32_t tag) noexcept {
+        return _internal::GetFieldUInt<uint32_t>(tiff, tag);
     }
 
     std::shared_ptr<TIFF> SafeTIFFOpen(const std::string& path, const char* mode) noexcept {
--- libdermtiff/src/util.hpp
+++ libdermtiff/src/util.hpp
@@ -10,26 +10,20 @@ typedef struct tiff TIFF;
 
 namespace ldt {
     namespace util {
+        // T: uint16_t and uint32_t are implemented.
+        // If could not get the field, 0 will be returned.
         template <typename T>
-        T GetField(TIFF* const tiff, uint32_t tag) noexcept {
-            if (tiff == nullptr) {
-                return T();
-            } else {
-                T value;
-                TIFFGetField(tiff, tag, &value);
-                return value;
-            }
-        }
+        T GetField(TIFF* const tiff, uint32_t tag) noexcept;
 
         template <typename T>
         std::optional<T> GetFieldOpt(TIFF* const tiff, uint32_t tag) noexcept {
             if (tiff == nullptr) {
                 return std::nullopt;
             } else {
-                if (T value; TIFFGetField(tiff, tag, &value) != 1) {
-                    return std::nullopt;
-                } else {
+                if (T value; TIFFGetField(tiff, tag, &value) == 1) {
                     return value;
+                } else {
+                    return std::nullopt;
                 }
             }
         }
