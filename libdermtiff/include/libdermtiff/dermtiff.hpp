7c5ddee20adb02b5412b37ff3ae82138198baaab
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2022-05-26 22:05:18 +0900

    release: bump version to 0.3.0

--- libdermtiff/include/libdermtiff/dermtiff.hpp
+++ libdermtiff/include/libdermtiff/dermtiff.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #define LIBDERMTIFF_VERSION_MAJOR 0
-#define LIBDERMTIFF_VERSION_MINOR 2
+#define LIBDERMTIFF_VERSION_MINOR 3
 #define LIBDERMTIFF_VERSION_PATCH 0
 #define LIBDERMTIFF_VERSION                                                                                            \
     (LIBDERMTIFF_VERSION_MAJOR * 10000 + LIBDERMTIFF_VERSION_MINOR * 100 + LIBDERMTIFF_VERSION_PATCH)
