b2ebcc1c0a003ca75fefe0e3a9035673f77ab485
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2022-07-08 16:54:31 +0900

    fix(Pencil): use char[] instead of std::string for name

--- libdermtiff/include/libdermtiff/dermtiff/pencil.hpp
+++ libdermtiff/include/libdermtiff/dermtiff/pencil.hpp
@@ -6,15 +6,25 @@
 
 namespace ldt {
     struct Pencil {
-        std::string name = "";
+        static const uint8_t MaxNameLength = 64;
+
+        char name[MaxNameLength + 1] = "\0";
         uint8_t r = 0, g = 0, b = 0, a = 255;
 
+        Pencil() = default;
+
+        Pencil(std::string_view _name);
+
+        Pencil(std::string_view _name, uint8_t _r, uint8_t _g, uint8_t _b, uint8_t _a = 255);
+
         static std::optional<Pencil> Parse(const std::string& str);
+
+        bool setName(std::string_view _name);
 
         std::optional<std::string> toString() const;
 
         bool operator==(const Pencil& other) const noexcept {
-            return name == other.name && r == other.r && g == other.g && b == other.b;
+            return strcmp(name, other.name) == 0 && r == other.r && g == other.g && b == other.b;
         }
 
         bool operator!=(const Pencil& other) const noexcept {
