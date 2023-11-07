eab49d607cc21b8921f02fd3a7d4cd4b4ecdeecf
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2023-11-07 18:19:59 +0900

    wip

--- libdermtiff/include/libdermtiff/dermtiff.hpp
+++ libdermtiff/include/libdermtiff/dermtiff.hpp
@@ -1,11 +1,5 @@
 #pragma once
 
-#define LIBDERMTIFF_VERSION_MAJOR 0
-#define LIBDERMTIFF_VERSION_MINOR 4
-#define LIBDERMTIFF_VERSION_PATCH 1
-#define LIBDERMTIFF_VERSION                                                                                            \
-    (LIBDERMTIFF_VERSION_MAJOR * 10000 + LIBDERMTIFF_VERSION_MINOR * 100 + LIBDERMTIFF_VERSION_PATCH)
-
 #include "dermtiff/derm_tiff.hpp"
 #include "dermtiff/io.hpp"
 #include "dermtiff/pencil.hpp"
--- libdermtiff/include/libdermtiff/dermtiff/common.hpp
+++ libdermtiff/include/libdermtiff/dermtiff/common.hpp
@@ -1,4 +1,21 @@
 #pragma once
 
+#define LIBDERMTIFF_VERSION_MAJOR 0
+#define LIBDERMTIFF_VERSION_MINOR 4
+#define LIBDERMTIFF_VERSION_PATCH 1
+#define LIBDERMTIFF_VERSION                                                                                            \
+    (LIBDERMTIFF_VERSION_MAJOR * 10000 + LIBDERMTIFF_VERSION_MINOR * 100 + LIBDERMTIFF_VERSION_PATCH)
+
+#define NUM_TO_STR(N) #N
+
 #define EXPORT extern "C" __declspec(dllexport)
 #define STDCALL __stdcall
+
+EXPORT const char* STDCALL GetVersion() {
+    return NUM_TO_STR(LIBDERMTIFF_VERSION_MAJOR) "." NUM_TO_STR(LIBDERMTIFF_VERSION_MINOR) "." NUM_TO_STR(
+        LIBDERMTIFF_VERSION_PATCH);
+}
+
+EXPORT const char* STDCALL GetVersionNum() {
+    return NUM_TO_STR(LIBDERMTIFF_VERSION);
+}
