dfde58dd09707de35f346f73f5913631e53f3acd
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2023-11-07 19:56:24 +0900

    fix

--- libdermtiff/include/libdermtiff/dermtiff/common.hpp
+++ libdermtiff/include/libdermtiff/dermtiff/common.hpp
@@ -4,8 +4,13 @@
 #define LIBDERMTIFF_VERSION_MINOR 4
 #define LIBDERMTIFF_VERSION_PATCH 1
 
+#ifdef _WIN32
 #define EXPORT extern "C" __declspec(dllexport)
 #define STDCALL __stdcall
+#else
+#define EXPORT extern "C"
+#define STDCALL
+#endif
 
 namespace ldt {
     EXPORT const char* STDCALL GetVersion();
