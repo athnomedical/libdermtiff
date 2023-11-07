a32ce66d6011b3cfbea2225a66923bd3604fd452
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2023-11-07 16:33:10 +0900

    wip

---
+++ libdermtiff/include/libdermtiff/dermtiff.cpp
@@ -0,0 +1,15 @@
+#include "dermtiff.hpp"
+
+#include <iostream>
+
+#include "dermtiff/io.hpp"
+#include "dermtiff/message.hpp"
+#include "dermtiff/pencil.hpp"
+#include "dermtiff/type.hpp"
+
+namespace ldt {
+    EXPORT int STDCALL example() {
+        std::cout << "Call example() and return 123." << std::endl;
+        return 123;
+    }
+}
--- libdermtiff/include/libdermtiff/dermtiff.hpp
+++ libdermtiff/include/libdermtiff/dermtiff.hpp
@@ -6,7 +6,9 @@
 #define LIBDERMTIFF_VERSION                                                                                            \
     (LIBDERMTIFF_VERSION_MAJOR * 10000 + LIBDERMTIFF_VERSION_MINOR * 100 + LIBDERMTIFF_VERSION_PATCH)
 
-#include "dermtiff/io.hpp"
-#include "dermtiff/message.hpp"
-#include "dermtiff/pencil.hpp"
-#include "dermtiff/type.hpp"
+#define EXPORT extern "C" __declspec(dllexport)
+#define STDCALL __stdcall
+
+namespace ldt {
+    EXPORT int STDCALL example();
+}
