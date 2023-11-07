376d857d90e98d82916493499ade024ba5184c42
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2023-11-07 18:32:11 +0900

    wip

--- libdermtiff/include/libdermtiff/dermtiff/common.hpp
+++ libdermtiff/include/libdermtiff/dermtiff/common.hpp
@@ -3,10 +3,6 @@
 #define LIBDERMTIFF_VERSION_MAJOR 0
 #define LIBDERMTIFF_VERSION_MINOR 4
 #define LIBDERMTIFF_VERSION_PATCH 1
-#define LIBDERMTIFF_VERSION                                                                                            \
-    (LIBDERMTIFF_VERSION_MAJOR * 10000 + LIBDERMTIFF_VERSION_MINOR * 100 + LIBDERMTIFF_VERSION_PATCH)
-
-#define NUM_TO_STR(N) #N
 
 #define EXPORT extern "C" __declspec(dllexport)
 #define STDCALL __stdcall
--- libdermtiff/src/common.cpp
+++ libdermtiff/src/common.cpp
@@ -1,12 +1,18 @@
 #include "common.hpp"
 
+#define STRINGFY2(s) #s
+#define STRINGFY(s) STRINGFY2(s)
+
+#define LIBDERMTIFF_VERSION                                                                                            \
+    (LIBDERMTIFF_VERSION_MAJOR * 10000 + LIBDERMTIFF_VERSION_MINOR * 100 + LIBDERMTIFF_VERSION_PATCH)
+
 namespace ldt {
     EXPORT const char* STDCALL GetVersion() {
-        return NUM_TO_STR(LIBDERMTIFF_VERSION_MAJOR) "." NUM_TO_STR(LIBDERMTIFF_VERSION_MINOR) "." NUM_TO_STR(
-            LIBDERMTIFF_VERSION_PATCH);
+        return STRINGFY(LIBDERMTIFF_VERSION_MAJOR) "." STRINGFY(LIBDERMTIFF_VERSION_MINOR) "." STRINGFY(
+            LIBDERMTIFF_VERSION_PATCH) ".";
     }
 
     EXPORT const char* STDCALL GetVersionNum() {
-        return NUM_TO_STR(LIBDERMTIFF_VERSION);
+        return STRINGFY(LIBDERMTIFF_VERSION);
     }
 }
