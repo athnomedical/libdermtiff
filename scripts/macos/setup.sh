b79a54c0af4070bb0f7029e27538846e23bacaa8
Ekyu Kondo <kondo.eque@gmail.com>
2023-11-11 14:02:34 +0900

    fix for mac

--- scripts/macos/setup.sh
+++ scripts/macos/setup.sh
@@ -1,11 +1,6 @@
 #!/bin/zsh
 running_path=`pwd`
 script_path=$(cd $(dirname $0); pwd)
-repo_path="${script_path}/../../"
-
-# Workaround for compiling error of libtiff
-script_path=$(cd $(dirname $0); pwd)
-# mv ${repo_path}/deps/libtiff/VERSION ${repo_path}/deps/libtiff/VERSION.txt
 
 # Build dependencies
 zsh ${script_path}/install_dependency.sh
